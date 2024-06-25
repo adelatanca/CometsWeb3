20cf609c6c1f9af05c9c90e385d8ab77ac01b6f2
adelatanca <adelaatancaa@gmail.com>
2024-06-25 20:22:22 +0300

    first project

---
+++ First_Project/generate-keypair.ts
@@ -0,0 +1,11 @@
+// import { Keypair } from "@solana/web3.js";
+// const keypair = Keypair.generate();
+// console.log(`✅ Generated keypair!`)
+
+import { Keypair } from "@solana/web3.js";
+
+const { publicKey, secretKey} = Keypair.generate();
+
+console.log(`The public key is: `, publicKey.toBase58());
+console.log(`The secret key is: `, secretKey);
+console.log(`✅ Finished!`);
